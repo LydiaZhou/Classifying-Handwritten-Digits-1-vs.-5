471ee25a4d96e96d4564c7cf212d1a5bea25ca3c
Lydia <zhouy13@rpi.edu>
2019-02-26 20:21:38 +0800

    3rd order polynomial transform

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Classifying-Handwritten-Digits-1-vs.-5
 Proceptron Learning Algorithm(PLA) Followed by Pocket;
 Linear Regression with least square method;
-Linear regression with Gradient Descent;
+Logistic regression with Gradient Descent;
 3rd Order Polynomial Transform
--- main.py
+++ main.py
@@ -9,10 +9,14 @@ with open("settings.yaml", 'r') as stream:
 
 # Vairable Declaration
 perceptronIndicator = data_loaded['perceptron']
-linear1Indicator = data_loaded['linearRegMinSquare']
-linear2Indicator = data_loaded['linearRegGradient']
+linearIndicator = data_loaded['linearReg']
+logisticIndicator = data_loaded['logisticReg']
 max_epoches = data_loaded['max_epoches']
 learning_rate = data_loaded['learning_rate']
+polyIndicator = data_loaded['3rdOrder']
+weight_poly = 0
+x_poly = 0
+weight = 0
 
 # Plotting function for generating the virsualization graph
 def plot(data):
@@ -24,6 +28,59 @@ def plot(data):
     plt.plot(fives_sym, fives_den, 'bo')
     plt.show()
 
+# Linear Regression with min square eq
+def linearReg(x, weight):
+    # Linear Regression with least square method
+    pseudoInverseX = np.dot(np.linalg.inv(np.dot(np.transpose(x), x)), np.transpose(x))
+    wLin = np.dot(pseudoInverseX, y)
+    hypothesis = np.dot(x, wLin)
+    # Convert float result in hypothesis into 1 or 0
+    hypothesis = np.asarray([int(element + 0.5) for element in hypothesis])
+    eTrain = ((hypothesis - trainLabel) ** 2).mean(axis=None)
+    return eTrain
+
+def perceptron():
+    # PLA(Perceptron) with pocket algorithm as improvement
+    weights = (1, 1)
+    minEtrain = 100
+    minWeight = weights
+    for iter in range(max_epoches):
+        for i in range(len(trainData)):
+            # If the x1*w1+x2*w2>0, the number should be five(label = 0); If the x1*w1+x2*w2<0, label =1
+            # else, re-balance the weights
+            calculatedLabel = trainData[i][1] * weights[0] + trainData[i][2] * weights[1]
+            if ((calculatedLabel > 0 and trainLabel[i] == 0) or (calculatedLabel < 0 and trainLabel[i] == 1)):
+                continue
+            else:
+                aLabel = calculatedLabel
+                aArray = np.asarray(trainData[i][1:])
+                a = learning_rate * calculatedLabel * np.asarray(trainData[i][1:]) / (
+                            trainData[i][1] ** 2 + trainData[i][2] ** 2)
+                weights -= a
+                # hypothesis = x1*w1+x2*w2
+                hypothesis = np.dot(np.asarray(trainData)[:, 1:], np.asarray(weights))
+                hypothesis = np.asarray([(0 if element > 0 else 1) for element in hypothesis])
+                eTrain = ((hypothesis - trainLabel) ** 2).mean(axis=None)
+                if eTrain < minEtrain:
+                    minEtrain = eTrain
+                    minWeight = weights
+    print("The training error is %.5f" % minEtrain)
+
+def logisticReg(x, weight):
+    # Logistic Regression with Graident Descent
+    tolerance = 0.02
+    epoch = 0
+    eTrain = 1
+    while (eTrain > tolerance and epoch < max_epoches):
+        z = np.dot(x, weight)
+        hypothesis = 1 / (1 + np.exp(-z))
+        # Update weight in the direction of the partial derivative
+        weight += learning_rate * np.dot(y - hypothesis, x) / len(trainData)
+        epoch += 1
+        eTrain = ((hypothesis - trainLabel) ** 2).mean(axis=None)
+    return eTrain
+
+
 if __name__=="__main__":
     # Extract the data from source text file first
     # Dict for plotting; Data for training(without label)
@@ -31,70 +88,39 @@ if __name__=="__main__":
     trainDict, trainData, trainLabel = digitsExtractor.extractFile("ZipDigits.train")
     # Plot
     plot(trainDict)
+    weight = np.zeros(3)
 
-    if (linear1Indicator):
-        # Linear Regression with least square method
+    if (polyIndicator):
+        # change x=(1, x1, x2) into (1, x1, x2, x1^2, x2^2, x1x2, x1^3, ...)
         x = np.asarray(trainData)
         y = np.array(trainLabel)
-        pseudoInverseX = np.dot(np.linalg.inv(np.dot(np.transpose(x),x)),np.transpose(x))
-        wLin = np.dot(pseudoInverseX, y)
-        hypothesis = np.dot(x, wLin)
-        # Convert float result in hypothesis into 1 or 0
-        hypothesis = np.asarray([int(element+0.5) for element in hypothesis])
-        eTrain = ((hypothesis - trainLabel)**2).mean(axis=None)
+        x_poly = np.empty((len(x),10))
+
+        for i in range(len(x)):
+            xi = x[i][1]
+            yi = x[i][2]
+            new_ele = [1, xi, yi, xi**2, yi**2, xi*yi, xi**3, yi**3, xi**2*yi, yi**2*xi]
+            x_poly[i] = np.asarray(new_ele)
+        weight_poly = np.zeros(10)
+
+    if (linearIndicator):
         print("==========Linear Regression============")
-        print("The training error is %.5f" %eTrain)
-        # print("The balanced weights is %.5f" %wLin)
+        if (polyIndicator):
+            eTrain_poly = linearReg(x_poly, weight_poly)
+            print("The training error with 3rd order polynormial is %.5f" %eTrain_poly)
+        eTrain = linearReg(x, weight)
+        print("The training error is %.5f" % eTrain)
 
     if (perceptronIndicator):
-        # PLA(Perceptron) with pocket algorithm as improvement
-        weights = (1, 1)
-        minEtrain = 100
-        minWeight = weights
-        for iter in range(max_epoches):
-            for i in range(len(trainData)):
-                # If the x1*w1+x2*w2>0, the number should be five(label = 0); If the x1*w1+x2*w2<0, label =1
-                # else, re-balance the weights
-                calculatedLabel = trainData[i][1]*weights[0]+trainData[i][2]*weights[1]
-                if ((calculatedLabel > 0 and trainLabel[i] == 0) or (calculatedLabel < 0 and trainLabel[i] == 1)):
-                    continue
-                else:
-                    aLabel = calculatedLabel
-                    aArray = np.asarray(trainData[i][1:])
-                    a = learning_rate * calculatedLabel * np.asarray(trainData[i][1:])/(trainData[i][1]**2+trainData[i][2]**2)
-                    weights -= a
-                    # hypothesis = x1*w1+x2*w2
-                    hypothesis = np.dot(np.asarray(trainData)[:, 1:], np.asarray(weights))
-                    hypothesis = np.asarray([(0 if element > 0 else 1) for element in hypothesis])
-                    eTrain = ((hypothesis - trainLabel) ** 2).mean(axis=None)
-                    if eTrain < minEtrain:
-                        minEtrain = eTrain
-                        minWeight = weights
         print("==========Perceptron with Pocket============")
-        print("The training error is %.5f" %minEtrain)
-        # print("The balanced weights is %.5f" %minWeight)
+        perceptron()
 
-    if (linear2Indicator):
-        # Linear Regression with Graident Descent
-        tolerance = 0.2
-        difference = 1
-        epoch = 0
-        x = np.asarray(trainData)
-        y = np.array(trainLabel)
-        weight = np.zeros(3)
-        while(difference > tolerance and epoch < max_epoches):
-            hypothesis = np.dot(x, weight)
-            difference = np.absolute(np.sum(y - hypothesis))
-            # Update weight in the direction of the partial derivative
-            step1 = len(trainData)
-            step2 = y - hypothesis
-            step3 = x
-            step4 = np.dot(y - hypothesis, x)
-            weight += learning_rate*np.dot(y - hypothesis, x)/len(trainData)/1000
-            epoch += 1
-            eTrain = ((hypothesis - trainLabel) ** 2).mean(axis=None)
-            print(difference)
+    if (logisticIndicator):
         print("==========Linear Regression with Gradient Descent============")
+        if (polyIndicator):
+            eTrain_poly = logisticReg(x_poly, weight_poly)
+            print("The training error with 3rd order polynormial is %.5f" %eTrain_poly)
+        eTrain = logisticReg(x, weight)
         print("The training error is %.5f" % eTrain)
 
 
